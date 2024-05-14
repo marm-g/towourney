99713e769af270dcdae84f466c70e1c9baa8b551
Reno DuBois <me@renodubois.com>
2024-05-13 22:43:43 -0500

    Add Bulma for styling, Prettier for code formatting

--- ui/src/lib/pocketbase.ts
+++ ui/src/lib/pocketbase.ts
@@ -1,13 +1,13 @@
-import Pocketbase from 'pocketbase';
+import Pocketbase from "pocketbase";
 
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 // TODO: Make this configurable per environment
-export const pb = new Pocketbase('http://127.0.0.1:8090')
+export const pb = new Pocketbase("http://127.0.0.1:8090");
 
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
-    console.log('authStore changed', auth);
-    currentUser.set(pb.authStore.model);
-})
+  console.log("authStore changed", auth);
+  currentUser.set(pb.authStore.model);
+});
