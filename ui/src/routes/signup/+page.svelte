629f1541bfd3da183d9b6c0c29bc8f8020575b62
sara <sarajtiffin@gmail.com>
2024-05-16 11:51:36 -0500

    WIP: add sign up page

---
+++ ui/src/routes/signup/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { currentUser, pb } from "$lib/pocketbase";
+
+  let email: string;
+  let password: string;
+  let confirmPassword: string;
+
+  async function signup() {
+    await pb.collection("users").create();
+  }
+</script>
+
+<main class="columns">
+  <section class="section column is-4 is-offset-4 is-large">
+    <form on:submit|preventDefault>
+      <div class="field">
+        <input
+          placeholder="email"
+          class="input"
+          type="text"
+          bind:value={email}
+        />
+      </div>
+      <div class="field">
+        <input
+          placeholder="password"
+          class="input"
+          type="password"
+          bind:value={password}
+        />
+      </div>
+      <div class="field">
+        <input
+          placeholder="confirm password"
+          class="input"
+          type="password"
+          bind:value={confirmPassword}
+        />
+      </div>
+    </form>
+  </section>
+</main>
